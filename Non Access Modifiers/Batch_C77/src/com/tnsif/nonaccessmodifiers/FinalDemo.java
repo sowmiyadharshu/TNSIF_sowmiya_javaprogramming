264fe828a100074383d5d7acd6636d6b1ed0f4fe
Sowmiya <Sowmiya@LAPTOP-PMFG7OHE>
2025-10-14 10:15:51 +0530

    initial commit

---
+++ Non Access Modifiers/Batch_C77/src/com/tnsif/nonaccessmodifiers/FinalDemo.java
@@ -0,0 +1,27 @@
+package com.tnsif.nonaccessmodifiers;
+
+final class FinalDemo {
+	
+	final int Max = 100;
+	
+	 final void display() {
+		System.out.println("Show the value of max:  "+Max);
+	}
+
+	public static void main(String[] args) {
+		FinalDemo fd = new FinalDemo();
+	//	fd.Max = 300;
+		fd.display();
+		
+		
+	}
+
+}
+
+class Child extends FinalDemo{
+	
+	
+	void display() {
+		System.out.println("Show the value of max:  "+Max);
+	}
+}
